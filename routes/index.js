1dcb66cb979cda7b9397d1e1be33892a5867c3dc
Yujunyi2016 <junyi.yu@careerbuilder.com>
2017-07-20 20:15:49 +0800

    add css

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   req.models.person.find({name:'SB'}, function (err, result) {
-    console.log(result[0].name)
+
   });
   res.render('index', { title: 'Express' });
 });
